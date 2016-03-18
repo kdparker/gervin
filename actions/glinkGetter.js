b52fb4b99682bfb0f9b54a3b54b826749e043e4e
Keegan Parker <keegandparker@gmail.com>
2016-03-17 21:10:55 -0700

    GLink Getter and Setter

---
+++ actions/glinkGetter.js
@@ -0,0 +1,58 @@
+var Action = require("./baseAction.js"),
+    async = require('async'),
+    inherits = require('util').inherits;
+  
+function Getter(gervin) {
+    // We rely on setter to create the database we use
+    Action.call(this, gervin);
+}
+  
+inherits(Getter, Action);
+  
+Getter.prototype.name = "g/ Getter";
+ 
+Getter.prototype.help = "... g/Query ... \n" +
+    "Checks to see if there's a g link for Query, and if there is, will add a message with the response";
+
+Getter.prototype.onMessageMatcher = function(gervin, msg) {
+    return msg.content.match(/(\s|^)g\/[^\s]/i);
+}
+
+Getter.prototype.getOutputForLabel = function(gervin, label, callback) {
+    gervin.db.all("SELECT response FROM glink_responses WHERE label = ?", label, function(err, rows) {
+        if (err)
+            return callback(err)
+        if (!rows.length) 
+            return callback(null, "")
+        return callback(null, "g/" + label + ": " + rows[0].response);
+    });
+}
+
+Getter.prototype.onMessage = function(gervin, msg) {
+    var self = this;
+
+    var labels = msg.content.match(/(\s|^)g\/[^\s]+/ig).map(function(link) {
+        return link.match(/(\s|^)g\/([^\s]+)/i)[2];
+    });
+
+    output = "";
+    async.eachSeries(labels, function(label, callback) {
+        self.getOutputForLabel(gervin, label, function(err, outputForLabel) {
+            if (err)
+                callback(err);
+            if (output.length)
+                output += "\n";
+            output += outputForLabel;
+            callback(err);
+        });
+    }, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        if (output.length) {
+            gervin.sendMessage(msg.channel, output);
+        }
+    });
+};
+
+module.exports = Getter
---
+++ actions/glinkSetter.js
@@ -0,0 +1,63 @@
+var SlashAction = require("./slashAction.js"),
+    async = require('async'),
+    inherits = require('util').inherits;
+  
+function Setter(gervin) {
+    SlashAction.call(this, gervin, true);
+
+    var db = gervin.db;
+    console.log("Waiting for db initialization for Setter..."); 
+    db.serialize(function () {
+        db.run(
+            "CREATE TABLE IF NOT EXISTS glink_responses (" + 
+                "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " + 
+                "label TEXT NOT NULL, " +
+                "response TEXT NOT NULL" +
+            ")"
+        );
+    });
+}
+  
+inherits(Setter, SlashAction);
+  
+Setter.prototype.name = "g/ Setter";
+Setter.prototype.commandId = "set";
+ 
+Setter.prototype.help = "/set QUERY1 QUERY2\n" +
+    "Assigns QUERY1 to QUERY2 (i.e. if g/QUERY1 is said in any message gervin will" +
+    " reply with QUERY2 (case-insensitive).\n" +
+    "Note: QUERY2 may contain spaces, but QUERY1 cannot";
+
+Setter.prototype.onMessage = function(gervin, msg) {
+    var self = this;
+    if (self.arguments.length < 2) {
+        gervin.replyAndDelete(
+            msg,
+            "please include two queries to set a label"
+        );
+        return;
+    }
+    var label = self.arguments[0].toLowerCase();
+    var response = self.arguments.slice(1).join(" ");
+    gervin.db.serialize(function() {
+        gervin.db.run(
+            "INSERT OR REPLACE INTO glink_responses (id, label, response) VALUES (" +
+            "(SELECT id FROM glink_responses WHERE label = ?), " + 
+            "?, " +
+            "?)",
+            label, label, 
+            response, 
+            function(err) {
+                if (err) 
+                    console.log(err);
+                return err;
+            }
+        );  
+        gervin.replyAndDelete(
+            msg,
+            "g/" + label + " is now set to " + response
+        );
+    });
+};
+
+module.exports = Setter
